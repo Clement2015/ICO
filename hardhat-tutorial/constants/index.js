149de1c5c2eaef247eb850d6e55605ddde697968
Mba <jmba@uj.ac.za>
2022-09-19 00:49:05 +0200

    This is my ICO sale

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x4Ae723B65802D4b8AFf8602d8ab65F8eF4a96893";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
